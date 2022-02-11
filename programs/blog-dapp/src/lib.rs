1f403c8f77c0023113824aa7ab0cd53832027c15
jackson1992121 <dantes0121@hotmail.com>
2022-02-11 16:02:58 +0800

    add blog create and update

--- programs/blog-dapp/src/lib.rs
+++ programs/blog-dapp/src/lib.rs
@@ -1,14 +1,69 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("ofNm1x4gP25RFrdC95gbmAeezF2rZo56194GViHtgtm");
 
 #[program]
 pub mod blog_dapp {
+    use std::str::from_utf8;
+
     use super::*;
     pub fn initialize(ctx: Context<Initialize>) -> ProgramResult {
+        let b_p_a = &mut ctx.accounts.blog_account;
+        b_p_a.authority = *ctx.accounts.authority.key;
+        Ok(())
+    }
+
+    pub fn make_post(
+        ctx: Context<MakePost>,
+        new_post: Vec<u8> // <--- our blog post data
+    ) -> ProgramResult {
+        let post = from_utf8(&new_post) // convert the array of bytes into a string slice
+            .map_err(|err| {
+            msg!("Invalid UTF-8, from byte {}", err.valid_up_to());
+            ProgramError::InvalidInstructionData
+        })?;
+        msg!(post); // msg!() is a Solana macro that prints string slices to the program log, which we can grab from the transaction block data
+
+        let b_acc = &mut ctx.accounts.blog_account;
+        b_acc.latest_post = new_post; // save the latest post in the account.
+        // past posts will be saved in transaction logs
+
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(
+        init, // 1. Hey Anchor, initialize an account with these details for me
+        payer = authority, // 2. See that authority Signer (pubkey) down there? They're paying for this
+        space = 8 // 3.A) all accounts need 8 bytes for the account discriminator prepended to the account
+        + 32 // 3.B) authority: Pubkey needs 32 bytes
+        + 566 // 3.C) latest_post: post bytes could need up to 566 bytes for the memo
+        // You have to do this math yourself, there's no macro for this
+    )]
+    pub blog_account: Account<'info, BlogAccount>, // 1. <--- initialize this account variable & add it to Context.accounts.blog_account can now be used above in our initialize function
+    pub authority: Signer<'info>, // 5. <--- let's name the account that signs this transaction "authority" and make it mutable so we can set the value to it in `initialize` function above
+    pub system_program: Program<'info, System>, // <--- Anchor boilerplate
+}
+
+#[derive(Accounts)]
+pub struct MakePost<'info> {
+    #[account(
+        mut, // we can make changes to this account
+        has_one = authority)] // the authority has signed this post, allowing it to happen
+    // this is here again because it holds that .latest_post field where our post is saved
+    pub blog_account: Account<'info, BlogAccount>, // <-- enable this account to also be used in the make_post function
+    // Also put authority here
+    // has_one = authority ensure it was provided as a function arg
+    // ensures the poster has the keys
+    // has to come after the Account statement above
+    // no mut this time, because we don't change authority when we post
+    pub authority: Signer<'info>
+}
+
+#[account]
+pub struct BlogAccount {
+    pub authority: Pubkey,    // save the posting authority to this authority field
+    pub latest_post: Vec<u8>, // <-- where the latest blog post will be stored
+}
